a5b786318c680a34ce8f9a55eb3d24188f5aeeb4
TSStudio <TSS@mail.tmysam.top>
2019-09-28 15:07:12 +0800

    s

---
+++ fus/title.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>qx
+using namespace std;
+
+int main(void){
+    freopen("title.in","r",stdin);
+    freopen("title.out","w",stdout);
+    char a;
+    int cnt=0;
+    while(cin>>a){
+        cnt++;
+        if(a=='\\') cnt-=2;
+        if(a==' ') cnt--;
+    }
+    cout<<cnt;
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
